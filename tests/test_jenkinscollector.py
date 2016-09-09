fd3dda80d00fe298fb17a907273608867297bbc5
u206123 <florian.seidl@sbb.ch>
2016-09-09 14:58:34 +0200

    Minor refactoring in test_jenkinscollector

--- tests/test_jenkinscollector.py
+++ tests/test_jenkinscollector.py
@@ -37,35 +37,26 @@ class TestJenkinsClient(TestCase):
             c.latest_build("myjob")
         self.assertEqual(1, c.http_client.open_and_read.call_count)
 
-class TestJenkinsCollector(TestCase):
+class TestJenkinsCollectorJobs(TestCase):
     job_name_success = "mvp.mct.vermittler-produkt.continuous"
     job_name_failed = "mvp.mct.vermittler-orchestrierung_commons.continuous"
     job_name_unstable = "kd.esta.integrate.template.was3.it"
     job_name_building = "kd.sid.sid-library-ios.continuous"
-    view_name_1 = "mvp/view/mct-new/view/mct-develop/view/continuous"
-    view_name_2 = "kd/view/esta.integrate"
-    view_name_3 = "pz/view/tip/view/tip-all"
-    view_name_nested = "mvp/view/zvs-drittgeschaeft"
-    view_name_nested_loop = "mvp/view/zvs-drittgeschaeft-fake-broken-with-loop"
-    view_name_building = "kd/view/sid"
-    view_name_depth_2 = "pz/view/touri"
     url = "https://ci.sbb.ch"
 
-
-    def do_collect_jobs(self, job_name):
+    def do_collect_jobs(self, job_name, mock_open_and_read=None):
         col = JenkinsCollector(self.url, job_names= (job_name, ))
-        retval = read(job_name)
-        col.jenkins.http_client.open_and_read = MagicMock(spec=(""), return_value=retval)
+        col.jenkins.http_client.open_and_read = mock_open_and_read if mock_open_and_read != None else self.mock_open_and_read
         status = col.collect()
         self.assertIsNotNone(status[job_name])
         return status
 
     def do_collect_jobs_error(self, job_name, error):
-        col = JenkinsCollector(self.url, job_names= (job_name, ))
-        col.jenkins.http_client.open_and_read = MagicMock(spec=(""),side_effect=error)
-        status = col.collect()
-        self.assertIsNotNone(status[job_name])
-        return status
+        return self.do_collect_jobs(job_name=job_name, mock_open_and_read = MagicMock(spec=(""),side_effect=error))
+
+    def mock_open_and_read(self, request_path, folder=""):
+        match = re.match("/job/(.*)/lastBuild/api/json", request_path)
+        return read(folder + to_filename(match.group(1).strip("/")))
 
     def test_request_status_ok(self):
         status = self.do_collect_jobs(self.job_name_success)
@@ -112,79 +103,13 @@ class TestJenkinsCollector(TestCase):
         status = self.do_collect_jobs_error("foo", URLError("kaputt"))
         self.assertEqual("error", status["foo"]["request_status"])
 
-    def do_collect_views(self, expected_nr_jobs, view_name, error=None):
-        col = JenkinsCollector(self.url, view_names = (view_name, ))
-        col.jenkins.http_client.open_and_read = MagicMock(spec=(""),
-                                                return_value= read(to_filename(view_name)) if not error else None,
-                                                side_effect = error)
-        builds = col.collect()
-        self.assertEqual(len(builds), expected_nr_jobs)
-        return builds
-
-    def test_collect_views_count(self):
-        self.do_collect_views(66, view_name=self.view_name_1)
-
-    def test_collect_views_http_error(self):
-        self.do_collect_views(1, view_name=self.view_name_1, error=HTTPError(self.url, 500, None, None, None))
-
-    def test_collect_views_url_error(self):
-        self.do_collect_views(1, view_name=self.view_name_1, error=URLError("kaputt"))
-
-    def test_collect_views_count_failure(self):
-        build = self.do_collect_views(66, view_name=self.view_name_1)
-        self.assertEquals(4, len([k for (k, v) in build.items() if "result" in v and v["result"] == "failure"]))
-
-    def test_collect_views_count_success(self):
-        build = self.do_collect_views(66, view_name=self.view_name_1)
-        self.assertEquals(62, len([k for (k, v) in build.items() if "result" in v and v["result"] == "success"]))
-
-    def test_collect_views_count_unstabe(self):
-        build = self.do_collect_views(9, view_name=self.view_name_2)
-        self.assertEquals(4, len([k for (k, v) in build.items() if "result" in v and v["result"] == "unstable"]))
-
-    def test_collect_views_count_success_2(self):
-        build = self.do_collect_views(9, view_name=self.view_name_2)
-        self.assertEquals(5, len([k for (k, v) in build.items() if "result" in v and v["result"] == "success"]))
-
-    def test_collect_views_count_disabled(self):
-        build = self.do_collect_views(10, view_name=self.view_name_3)
-        self.assertEquals(1, len([k for (k, v) in build.items() if v["request_status"] == "not_found"]))
-
-    def test_collect_views_count_success_3(self):
-        build = self.do_collect_views(10, view_name=self.view_name_3)
-        self.assertEquals(9, len([k for (k, v) in build.items() if "result" in v and v["result"] == "success"]))
-
-    def test_collect_jobs_and_views(self):
-        col = JenkinsCollector(self.url, job_names= (self.job_name_success, self.job_name_failed), view_names=(self.view_name_2, ))
-        content_by_key = {
-                            self.job_name_success : read(self.job_name_success),
-                            self.job_name_failed :read(self.job_name_failed),
-                            self.view_name_2 : read(self.view_name_2.replace("/", "__"))
-        }
-        col.jenkins.http_client.open_and_read = Mock(spec=(""), side_effect=lambda x : [content_by_key[k] for k in content_by_key if k in x][0])
-        status = col.collect()
-        self.assertEquals(11, len(status))
-
-
-    def test_build_request_status_http_error(self):
-        status = self.do_collect_views(1, "foo", HTTPError("", 500, "kaputt", None, None))
-        self.assertEqual(status["foo"]["request_status"], "error")
-
     def test_build_result_not_building(self):
         status = self.do_collect_jobs(self.job_name_success)
         self.assertFalse(status[self.job_name_success]["building"])
 
-    def test_build_result_view_not_building(self):
-        build = self.do_collect_views(22, view_name=self.view_name_building)
-        self.assertEquals(0, len([k for (k, v) in build.items() if "buidling" in v and v["building"]]))
-
     def test_build_result_building(self):
         status = self.do_collect_jobs(self.job_name_building)
         self.assertTrue(status[self.job_name_building]["building"])
-
-    def test_collect_view_building(self):
-        build = self.do_collect_views(22, view_name=self.view_name_building)
-        self.assertEquals(6, len([k for (k, v) in build.items() if "building" in v and v["building"]]))
 
     def test_build_result_building_no_result(self):
         status = self.do_collect_jobs(self.job_name_building)
@@ -216,22 +141,72 @@ class TestJenkinsCollector(TestCase):
         self.assertEquals("failure", status[self.job_name_building]["result"])
         self.assertFalse(status[self.job_name_building]["building"])
 
-    def test_nested_view(self):
-        col = JenkinsCollector(self.url, view_names = (self.view_name_nested, ))
-        col.jenkins.http_client.open_and_read = self.mock_open_and_read_for_nested_view
-        build = col.collect()
-        self.assertEqual(len(build), 124)
 
-    def test_nested_view_loop(self):
-        col = JenkinsCollector(self.url, view_names = (self.view_name_nested_loop, ))
-        col.jenkins.http_client.open_and_read = self.mock_open_and_read_for_nested_view
-        build = col.collect()
-        self.assertEqual(len(build), 124)
+class TestJenkinsCollectorViews(TestCase):
+    view_name_1 = "mvp/view/mct-new/view/mct-develop/view/continuous"
+    view_name_2 = "kd/view/esta.integrate"
+    view_name_3 = "pz/view/tip/view/tip-all"
+    view_name_building = "kd/view/sid"
+    view_name_depth_2 = "pz/view/touri"
+    url = "https://ci.sbb.ch"
 
-    def mock_open_and_read_for_nested_view(self, request_path):
+    def do_collect_views(self, expected_nr_jobs, view_name, mock_open_and_read=None):
+        col = JenkinsCollector(self.url, view_names = (view_name, ))
+        col.jenkins.http_client.open_and_read = mock_open_and_read if mock_open_and_read != None else self.mock_open_and_read
+        builds = col.collect()
+        self.assertEqual(len(builds), expected_nr_jobs)
+        return builds
+
+    def do_collect_views_error(self, expected_nr_jobs, view_name, error):
+        return self.do_collect_views(expected_nr_jobs=expected_nr_jobs,
+                                        view_name = view_name,
+                                        mock_open_and_read = MagicMock(spec=(""), side_effect = error))
+
+
+    def mock_open_and_read(self, request_path, folder=""):
         match = re.match("/view/(.*)/api/json", request_path)
-        view_name = match.group(1).strip("/")
-        return read("nested_view/" + to_filename(view_name))
+        return read(folder + to_filename(match.group(1).strip("/")))
+
+    def test_collect_views_count(self):
+        self.do_collect_views(66, view_name=self.view_name_1)
+
+    def test_collect_views_http_error(self):
+        self.do_collect_views_error(1, view_name=self.view_name_1, error=HTTPError(self.url, 500, None, None, None))
+
+    def test_collect_views_url_error(self):
+        self.do_collect_views_error(1, view_name=self.view_name_1, error=URLError("kaputt"))
+
+    def test_collect_views_count_failure(self):
+        build = self.do_collect_views(66, view_name=self.view_name_1)
+        self.assertEquals(4, len([k for (k, v) in build.items() if "result" in v and v["result"] == "failure"]))
+
+    def test_collect_views_count_success(self):
+        build = self.do_collect_views(66, view_name=self.view_name_1)
+        self.assertEquals(62, len([k for (k, v) in build.items() if "result" in v and v["result"] == "success"]))
+
+    def test_collect_views_count_unstabe(self):
+        build = self.do_collect_views(9, view_name=self.view_name_2)
+        self.assertEquals(4, len([k for (k, v) in build.items() if "result" in v and v["result"] == "unstable"]))
+
+    def test_collect_views_count_success_2(self):
+        build = self.do_collect_views(9, view_name=self.view_name_2)
+        self.assertEquals(5, len([k for (k, v) in build.items() if "result" in v and v["result"] == "success"]))
+
+    def test_collect_views_count_disabled(self):
+        build = self.do_collect_views(10, view_name=self.view_name_3)
+        self.assertEquals(1, len([k for (k, v) in build.items() if v["request_status"] == "not_found"]))
+
+    def test_collect_views_count_success_3(self):
+        build = self.do_collect_views(10, view_name=self.view_name_3)
+        self.assertEquals(9, len([k for (k, v) in build.items() if "result" in v and v["result"] == "success"]))
+
+    def test_build_request_status_http_error(self):
+        status = self.do_collect_views_error(1, "foo", HTTPError("", 500, "kaputt", None, None))
+        self.assertEqual(status["foo"]["request_status"], "error")
+
+    def test_collect_view_building(self):
+        build = self.do_collect_views(22, view_name=self.view_name_building)
+        self.assertEquals(6, len([k for (k, v) in build.items() if "building" in v and v["building"]]))
 
     def test_collect_view_with_numbers(self):
         build = self.do_collect_views(6, view_name=self.view_name_depth_2)
@@ -248,6 +223,44 @@ class TestJenkinsCollector(TestCase):
     def test_collect_view_without_numbers(self):
         build = self.do_collect_views(66, view_name=self.view_name_1)
         self.assertEquals(0, len([k for (k, v) in build.items() if "number" in v and v["number"]]))
+
+    def test_build_result_view_not_building(self):
+        build = self.do_collect_views(22, view_name=self.view_name_building)
+        self.assertEquals(0, len([k for (k, v) in build.items() if "buidling" in v and v["building"]]))
+
+class TestJenkinsCollectorNestedViews(TestCase):
+    view_name_nested = "mvp/view/zvs-drittgeschaeft"
+    view_name_nested_loop = "mvp/view/zvs-drittgeschaeft-fake-broken-with-loop"
+
+    def setUp(self):
+        self.testViews = TestJenkinsCollectorViews()
+
+    def mock_open_and_read_for_nested_view(self, request_path):
+        return self.testViews.mock_open_and_read(request_path, folder="nested_view/")
+
+    def test_nested_view(self):
+        self.testViews.do_collect_views(124, view_name=self.view_name_nested, mock_open_and_read=self.mock_open_and_read_for_nested_view)
+
+    def test_nested_view_loop(self):
+        self.testViews.do_collect_views(124, view_name=self.view_name_nested_loop, mock_open_and_read=self.mock_open_and_read_for_nested_view)
+
+
+class TestJenkinsCollectorJobsAndViews(TestCase):
+
+    def setUp(self):
+        self.testJobs = TestJenkinsCollectorJobs()
+        self.testViews = TestJenkinsCollectorViews()
+
+    def test_collect_jobs_and_views(self):
+        col = JenkinsCollector(self.testJobs.url, job_names= (self.testJobs.job_name_success, self.testJobs.job_name_failed), view_names=(self.testViews.view_name_2, ))
+        content_by_key = {
+            self.testJobs.job_name_success : read(to_filename(self.testJobs.job_name_success)),
+            self.testJobs.job_name_failed : read(to_filename(self.testJobs.job_name_failed)),
+            self.testViews.view_name_2 : read(to_filename(self.testViews.view_name_2))
+        }
+        col.jenkins.http_client.open_and_read = Mock(spec=(""), side_effect=lambda x : [content_by_key[k] for k in content_by_key if k in x][0])
+        status = col.collect()
+        self.assertEquals(11, len(status))
 
 if __name__ == '__main__':
     main()
