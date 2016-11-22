11f960d6962b2a260f25653f13f48fdeee5d6d09
u206123 <florian.seidl@sbb.ch>
2016-11-22 14:17:24 +0100

    new feature: energenie output color assignements are configurable now

--- src/energenieoutput.py
+++ src/energenieoutput.py
@@ -8,33 +8,40 @@ import logging
 
 default_repeat_every = 15
 
+default_colors={1 : "red", 2: "yellow", 3: "green", 4: "red"}
+
 logger = logging.getLogger(__name__)
 
 # output to the Energenie Power socket
 # treats the Energenie as an Ampel, see AbstractBuildAmpel for the logic.
 # uses the sispmctl command line tool (as user).
 # The Energenie has 4 controllable outputs, 1-4.
+# assignment is configurable, default is:
 #   1: red
 #   2: yellow
 #   3: green
 #   4: red (second red for instance for a rotating light)
+# colors not assigned will not be switched
 # both red will be switched together
 def create(configuration, aesKey=None):
     return EnergenieBuildAmpel(device_nr=configuration.get("deviceNr", None),
                                signal_error_threshold=configuration.get("signalErrorThreshold", default_signal_error_threshold),
                                repeat_every=configuration.get("repeatEvery", default_repeat_every),
-                               build_filter_pattern=configuration.get("buildFilterPattern", None))
+                               build_filter_pattern=configuration.get("buildFilterPattern", None),
+                               colors=configuration.get("colors", default_colors))
 
 
 class EnergenieBuildAmpel(AbstractBuildAmpel):
     """ control energenie according to build status """
 
-    def __init__(self, device_nr=None, signal_error_threshold=default_signal_error_threshold, repeat_every=default_repeat_every, build_filter_pattern=None):
+    def __init__(self, device_nr=None, signal_error_threshold=default_signal_error_threshold, repeat_every=default_repeat_every, build_filter_pattern=None, colors=default_colors):
         super(EnergenieBuildAmpel, self).__init__(signal_error_threshold=signal_error_threshold, build_filter_pattern=build_filter_pattern)
         self.energenie = Energenie(device_nr=device_nr, repeat_every=repeat_every)
+        self.colors=colors
 
     def signal(self, red, yellow, green, flash=False): # igonre flash, this feature is not supported
-        self.energenie.switch(socket_1=red, socket_2=yellow, socket_3=green, socket_4=red)
+        signal=locals()
+        self.energenie.switch([(k, signal[v]) for k,v in self.colors.items()])
 
 class Energenie():
     """ control the energenie socket using the sispmctl script """
@@ -45,8 +52,9 @@ class Energenie():
         self.repeat_every=repeat_every
         self.__repeat_count = 0
 
-    def switch(self, socket_1=False, socket_2=False, socket_3=False, socket_4=False):
-        self.__switch_if_changed__((1, socket_1), (2, socket_2), (3, socket_3), (4, socket_4))
+    def switch(self, socket_on):
+        if socket_on:
+            self.__switch_if_changed__(*socket_on)
 
     def __switch_if_changed__(self, *socket_on):
         if socket_on != self.__last_state or self.__repeat_count >= self.repeat_every:
