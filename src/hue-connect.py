d3247941ad56443fc6db5599db614debcb5a403c
u208490 <urs.beeli@sbb.ch>
2017-12-07 16:05:40 +0100

    PHILIPS HUE support is functional

---
+++ src/hue-connect.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import os,sys
+from phue import Bridge
+
+def wait_key():
+    ''' Wait for a key press on the console and return it. '''
+    result = None
+    if os.name == 'nt':
+        import msvcrt
+        result = msvcrt.getch()
+    else:
+        import termios
+        fd = sys.stdin.fileno()
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        try:
+            result = sys.stdin.read(1)
+        except IOError:
+            pass
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+
+    return result
+
+ip = input("Enter the IP address of the hue bridge: ")
+
+print("Press the connect button on your hue controller and then press enter.")
+wait_key()
+
+b = Bridge(ip)
+
+# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
+b.connect()
+
+print("Configuration has succesfully been written to ~/.python_hue")
