40960d90f9d54f2943954eadb9c6398a9a57bbab
u206123 <florian.seidl@sbb.ch>
2017-12-10 18:45:35 +0100

    fix: test for config file

--- tests/test_configutil.py
+++ tests/test_configutil.py
@@ -8,6 +8,7 @@ import os
 
 @patch("os.path.expanduser", Mock(spec=(""), return_value="home"))
 class ConfigUtilTest(TestCase):
+    filename="bilbla_42_foobar_gibitsnicht_sowieso.blo"
     key = b'\n\xb5\x86\x92%\x97A.[\xea-A\x1f\xfcW\x9cEQ\xf0\x81\xaa\x8a\xaf`3\xa2\x0f\xae&\xf7\x88\x8f'
     passwordEncrypted = "6qcvb0Kt0FSU"
     passwordDecrypted = "topsecret"
@@ -27,18 +28,22 @@ class ConfigUtilTest(TestCase):
     def test_encrypt_decrypt(self):
         self.assertEqual(decrypt(encrypt(self.passwordDecrypted, self.key),self.key), self.passwordDecrypted)
 
-    def test_find_config_in_path(self):
+    def test_find_config_in_path_directly(self):
         self.mock_os_isfile(True)
-        self.assertEqual("home/cimon/bilbla.blo", find_config_file_path("bilbla.blo"))
+        self.assertEqual(self.filename, find_config_file_path(self.filename))
+
+    def test_find_config_in_path(self):
+        os.path.isfile=Mock(spec=(""), side_effect=[False, True])
+        self.assertEqual("home/cimon/%s" % self.filename, find_config_file_path(self.filename).replace("\\", "/"))
 
     def test_find_config_not_in_path_mandatory(self):
         self.mock_os_isfile(False)
         with self.assertRaises(Exception):
-            find_config_file_path("bilbla.blo")
+            find_config_file_path(self.filename)
 
     def test_find_config_not_in_path_optional(self):
         self.mock_os_isfile(False)
-        self.assertIsNone(find_config_file_path("bilbla.blo", optional=True))
+        self.assertIsNone(find_config_file_path(self.filename, optional=True))
 
     def mock_os_isfile(self, found):
         os.path.isfile = Mock(spec=(""), return_value=found)
