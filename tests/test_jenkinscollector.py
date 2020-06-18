0d700e9ceb8aca36ebd327f67d861d53aa39eb6b
u206123 <florian.seidl@sbb.ch>
2020-06-19 00:59:29 +0200

    refactor: jenkinscollector wiht 3 different reades

--- tests/test_jenkinscollector.py
+++ tests/test_jenkinscollector.py
@@ -1,23 +1,25 @@
 __author__ = 'florianseidl'
 
-import env
-from jenkinscollector import JenkinsClient, JenkinsCollector
-from collector import HttpClient
-from urllib.request import HTTPError, URLError
+import os
+import re
+from datetime import datetime
 from unittest import TestCase, main
 from unittest.mock import MagicMock, Mock
-import os
-from datetime import datetime
-import re
-from cimon import Health,RequestStatus
+from urllib.error import HTTPError, URLError
+
+from cimon import Health, RequestStatus
+from collector import HttpClient
+from jenkinscollector import JenkinsClient, JenkinsCollector
 
 
 def read(file_name):
     with open("%s/testdata/%s" % (os.path.dirname(__file__), file_name), encoding='utf-8') as f:
         return f.read()
 
+
 def to_filename(name):
     return name.replace("/", "__")
+
 
 class TestJenkinsClient(TestCase):
     json_str = '{ "foo": "bar" }'
@@ -26,7 +28,7 @@ class TestJenkinsClient(TestCase):
         c = JenkinsClient(HttpClient("http://foo.bar"))
         c.http_client.open_and_read = MagicMock(spec=(""), return_value=self.json_str)
         res = c.latest_build("myjob")
-        self.assertEqual(res, {"foo" : "bar"})
+        self.assertEqual(res, {"foo": "bar"})
         c.http_client.open_and_read.assert_called_with("/job/myjob/lastBuild/api/json?depth=0")
 
     def test_http_exception_500(self):
@@ -37,6 +39,7 @@ class TestJenkinsClient(TestCase):
             c.latest_build("myjob")
         self.assertEqual(1, c.http_client.open_and_read.call_count)
 
+
 class TestJenkinsCollectorJobs(TestCase):
     job_name_success = "mvp.mct.vermittler-produkt.continuous"
     job_name_failed = "mvp.mct.vermittler-orchestrierung_commons.continuous"
@@ -46,14 +49,15 @@ class TestJenkinsCollectorJobs(TestCase):
     url = "https://ci.sbb.ch"
 
     def do_collect_jobs(self, job_name, mock_open_and_read=None):
-        col = JenkinsCollector(self.url, job_names= (job_name, ))
-        col.jenkins.http_client.open_and_read = mock_open_and_read if mock_open_and_read != None else self.mock_open_and_read
+        col = JenkinsCollector(mock_jenkins_client(self.url, mock_open_and_read if mock_open_and_read != None else self.mock_open_and_read),
+                               self.url,
+                               job_names=(job_name,))
         status = col.collect()
-        self.assertIsNotNone(status[("ci.sbb.ch",job_name)])
+        self.assertIsNotNone(status[("ci.sbb.ch", job_name)])
         return status
 
     def do_collect_jobs_error(self, job_name, error):
-        return self.do_collect_jobs(job_name=job_name, mock_open_and_read = MagicMock(spec=(""),side_effect=error))
+        return self.do_collect_jobs(job_name=job_name, mock_open_and_read=MagicMock(spec=(""), side_effect=error))
 
     def mock_open_and_read(self, request_path, folder=""):
         match = re.match("/job/(.*)/lastBuild/api/json", request_path)
@@ -119,16 +123,18 @@ class TestJenkinsCollectorJobs(TestCase):
         self.assertEqual(Health.OTHER, status[("ci.sbb.ch", self.job_name_building)].health)
 
     def test_build_job_first_success_then_building(self):
-        col = JenkinsCollector(self.url, job_names= (self.job_name_building, ))
-        filter=lambda x : x.replace("\"building\":true", "\"building\":false").replace("\"result\":null", "\"result\":\"SUCCESS\"")
-        col.jenkins.http_client.open_and_read = MagicMock(spec=(""), return_value = filter(read(self.job_name_building)))
+        filter = lambda x: x.replace("\"building\":true", "\"building\":false").replace("\"result\":null",
+                                                                                        "\"result\":\"SUCCESS\"")
+        col = JenkinsCollector(mock_jenkins_client(self.url, MagicMock(spec=(""), return_value=filter(read(self.job_name_building)))),
+                               self.url,
+                               job_names=(self.job_name_building,))
         # first request - not building yet
         status = col.collect()
         self.assertEqual(Health.HEALTHY, status[("ci.sbb.ch", self.job_name_building)].health)
         self.assertFalse(status[("ci.sbb.ch", self.job_name_building)].active)
 
         # second request - building now
-        col.jenkins.http_client.open_and_read = MagicMock(spec=(""), return_value = read(self.job_name_building))
+        col.job_reader.jenkins.http_client.open_and_read = MagicMock(spec=(""), return_value = read(self.job_name_building))
         status = col.collect()
         self.assertEqual(Health.HEALTHY, status[("ci.sbb.ch", self.job_name_building)].health)
         self.assertTrue(status[("ci.sbb.ch", self.job_name_building)].active)
@@ -138,17 +144,21 @@ class TestJenkinsCollectorJobs(TestCase):
         col = self.test_build_job_first_success_then_building()
 
         # third request - failed
-        filter=lambda x : x.replace("\"building\":true", "\"building\":false").replace("\"result\":null", "\"result\":\"FAILURE\"")
-        col.jenkins.http_client.open_and_read = MagicMock(spec=(""), return_value = filter(read(self.job_name_building)))
+        filter = lambda x: x.replace("\"building\":true", "\"building\":false").replace("\"result\":null",
+                                                                                        "\"result\":\"FAILURE\"")
+        col.job_reader.jenkins.http_client.open_and_read = MagicMock(spec=(""), return_value=filter(read(self.job_name_building)))
         status = col.collect()
         self.assertEqual(Health.SICK, status[("ci.sbb.ch", self.job_name_building)].health)
         self.assertFalse(status[("ci.sbb.ch", self.job_name_building)].active)
 
     def test_job_name_from_url(self):
-        col = JenkinsCollector(self.url, job_names= (self.job_name_noname, ), job_name_from_url_pattern='https://ci.sbb.ch/job/(.+)/\d+/')
-        col.jenkins.http_client.open_and_read = self.mock_open_and_read
+        col = JenkinsCollector(mock_jenkins_client(self.url, self.mock_open_and_read),
+                               self.url,
+                               job_names=(self.job_name_noname,),
+                               job_name_from_url_pattern='https://ci.sbb.ch/job/(.+)/\d+/')
         status = col.collect()
-        self.assertIsNotNone(status[("ci.sbb.ch",'pt.cisi.orga/job/common-check/job/develop')])
+        self.assertIsNotNone(status[("ci.sbb.ch", 'pt.cisi.orga/job/common-check/job/develop')])
+
 
 class TestJenkinsCollectorViews(TestCase):
     view_name_1 = "mvp/view/mct-new/view/mct-develop/view/continuous"
@@ -160,17 +170,17 @@ class TestJenkinsCollectorViews(TestCase):
     url = "https://ci.sbb.ch"
 
     def do_collect_views(self, expected_nr_jobs, view_name, mock_open_and_read=None):
-        col = JenkinsCollector(self.url, view_names = (view_name, ))
-        col.jenkins.http_client.open_and_read = mock_open_and_read if mock_open_and_read != None else self.mock_open_and_read
+        col = JenkinsCollector(jenkins=mock_jenkins_client(self.url, mock_open_and_read if mock_open_and_read != None else self.mock_open_and_read),
+                               base_url=self.url,
+                               view_names=(view_name,))
         builds = col.collect()
         self.assertEqual(len(builds), expected_nr_jobs)
         return builds
 
     def do_collect_views_error(self, expected_nr_jobs, view_name, error):
         return self.do_collect_views(expected_nr_jobs=expected_nr_jobs,
-                                        view_name = view_name,
-                                        mock_open_and_read = MagicMock(spec=(""), side_effect = error))
-
+                                     view_name=view_name,
+                                     mock_open_and_read=MagicMock(spec=(""), side_effect=error))
 
     def mock_open_and_read(self, request_path, folder=""):
         match = re.match("/view/(.*)/api/json", request_path)
@@ -238,10 +248,13 @@ class TestJenkinsCollectorViews(TestCase):
         self.assertEqual(6, len([k for (k, v) in build.items() if v.active]))
 
     def test_job_name_from_url(self):
-        col = JenkinsCollector(self.url, view_names = (self.view_name_noname, ), job_name_from_url_pattern='https://ci.sbb.ch/job/(.+)')
-        col.jenkins.http_client.open_and_read = self.mock_open_and_read
+        col = JenkinsCollector(mock_jenkins_client(self.url, self.mock_open_and_read),
+                               self.url,
+                               view_names=(self.view_name_noname,),
+                               job_name_from_url_pattern='https://ci.sbb.ch/job/(.+)')
         builds = col.collect()
         print(str(builds))
+
 
 class TestJenkinsCollectorNestedViews(TestCase):
     view_name_nested = "mvp/view/zvs-drittgeschaeft"
@@ -254,10 +267,13 @@ class TestJenkinsCollectorNestedViews(TestCase):
         return self.testViews.mock_open_and_read(request_path, folder="nested_view/")
 
     def test_nested_view(self):
-        self.testViews.do_collect_views(124, view_name=self.view_name_nested, mock_open_and_read=self.mock_open_and_read_for_nested_view)
+        self.testViews.do_collect_views(124, view_name=self.view_name_nested,
+                                        mock_open_and_read=self.mock_open_and_read_for_nested_view)
 
     def test_nested_view_loop(self):
-        self.testViews.do_collect_views(124, view_name=self.view_name_nested_loop, mock_open_and_read=self.mock_open_and_read_for_nested_view)
+        self.testViews.do_collect_views(124, view_name=self.view_name_nested_loop,
+                                        mock_open_and_read=self.mock_open_and_read_for_nested_view)
+
 
 class TestJenkinsCollectorJobsAndViews(TestCase):
 
@@ -266,18 +282,23 @@ class TestJenkinsCollectorJobsAndViews(TestCase):
         self.testViews = TestJenkinsCollectorViews()
 
     def test_collect_jobs_and_views(self):
-        col = JenkinsCollector(self.testJobs.url, job_names= (self.testJobs.job_name_success, self.testJobs.job_name_failed), view_names=(self.testViews.view_name_2, ))
         content_by_key = {
-            self.testJobs.job_name_success : read(to_filename(self.testJobs.job_name_success)),
-            self.testJobs.job_name_failed : read(to_filename(self.testJobs.job_name_failed)),
-            self.testViews.view_name_2 : read(to_filename(self.testViews.view_name_2))
+            self.testJobs.job_name_success: read(to_filename(self.testJobs.job_name_success)),
+            self.testJobs.job_name_failed: read(to_filename(self.testJobs.job_name_failed)),
+            self.testViews.view_name_2: read(to_filename(self.testViews.view_name_2))
         }
-        col.jenkins.http_client.open_and_read = Mock(spec=(""), side_effect=lambda x : [content_by_key[k] for k in content_by_key if k in x][0])
+        col = JenkinsCollector(jenkins=mock_jenkins_client(self.testJobs.url, Mock(spec=(""), side_effect=lambda x:
+        [content_by_key[k] for k in content_by_key if k in x][0])),
+                               base_url=self.testJobs.url,
+                               job_names=(self.testJobs.job_name_success, self.testJobs.job_name_failed),
+                               view_names=(self.testViews.view_name_2,))
         status = col.collect()
         self.assertEqual(11, len(status))
 
+
 class TestJenkinsCollectorFolders(TestCase):
     folder_name_1 = "PN_ES"
+    folder_name_2 = "pt.cisi.orga"
     url = "https://ci.sbb.ch"
 
     def test_collect_folder(self):
@@ -294,21 +315,37 @@ class TestJenkinsCollectorFolders(TestCase):
 
     def test_collect_folder_sick(self):
         build = self.do_collect_folder(self.folder_name_1)
-        self.assertEqual(Health.SICK, build[("ci.sbb.ch", "PN_ES/aps-safety-logic/feature/refactorocs_attempt1")].health)
+        self.assertEqual(Health.SICK,
+                         build[("ci.sbb.ch", "PN_ES/aps-safety-logic/feature/refactorocs_attempt1")].health)
 
     def test_collect_folder_active(self):
         build = self.do_collect_folder(self.folder_name_1)
         self.assertTrue(build[("ci.sbb.ch", "PN_ES/aps-safety-logic/feature/refactorocs_attempt1")].active)
         self.assertFalse(build[("ci.sbb.ch", "PN_ES/aps-safety-logic/master")].active)
 
+    def test_collect_folder_number(self):
+        build = self.do_collect_folder(self.folder_name_2)
+        self.assertEqual(55, build[("ci.sbb.ch", "pt.cisi.orga/angebot/develop")].number)
+
+    def test_collect_folder_duration(self):
+        build = self.do_collect_folder(self.folder_name_2)
+        self.assertEqual(1209369, build[("ci.sbb.ch", "pt.cisi.orga/angebot/develop")].duration)
+
+    def test_collect_folder_timestamp(self):
+        build = self.do_collect_folder(self.folder_name_2)
+        self.assertEqual(datetime.fromtimestamp(1592443260.226),
+                         build[("ci.sbb.ch", "pt.cisi.orga/angebot/develop")].timestamp)
+
     def do_collect_folder(self, folder_name):
-        col = JenkinsCollector(self.url, folder_names = (folder_name, ))
-        col.jenkins.http_client.open_and_read = self.mock_open_and_read
+        col = JenkinsCollector(mock_jenkins_client(self.url, self.mock_open_and_read),
+                               self.url,
+                               folder_names=(folder_name,))
         return col.collect()
 
     def mock_open_and_read(self, request_path):
         match = re.match("/job/(.*)/api/json", request_path)
         return read("folder/" + to_filename(match.group(1).strip("/")))
+
 
 class TestJenkinsCollectorMultibranch(TestCase):
     multibranch_pipeline_name = 'mvp.bats.vermittler-automat-tvm.ui-tests'
@@ -320,24 +357,34 @@ class TestJenkinsCollectorMultibranch(TestCase):
 
     def test_collect_multibranch_pipeline_healthy(self):
         build = self.do_collect_multibranch_pipeline(self.multibranch_pipeline_name)
-        self.assertEqual(Health.HEALTHY, build[("ci.sbb.ch", "mvp.bats.vermittler-automat-tvm.ui-tests/develop")].health)
+        self.assertEqual(Health.HEALTHY,
+                         build[("ci.sbb.ch", "mvp.bats.vermittler-automat-tvm.ui-tests/develop")].health)
 
     def test_collect_multibranch_pipeline_request_status(self):
         build = self.do_collect_multibranch_pipeline(self.multibranch_pipeline_name)
-        self.assertEqual(RequestStatus.OK, build[("ci.sbb.ch", "mvp.bats.vermittler-automat-tvm.ui-tests/develop")].request_status)
+        self.assertEqual(RequestStatus.OK,
+                         build[("ci.sbb.ch", "mvp.bats.vermittler-automat-tvm.ui-tests/develop")].request_status)
 
     def test_collect_multibranch_pipeline_active(self):
         build = self.do_collect_multibranch_pipeline(self.multibranch_pipeline_name)
         self.assertFalse(build[("ci.sbb.ch", "mvp.bats.vermittler-automat-tvm.ui-tests/bugfix/20.1.3")].active)
 
     def do_collect_multibranch_pipeline(self, multibranch_pipeline_name):
-        col = JenkinsCollector(self.url, multibranch_pipeline_names = (multibranch_pipeline_name, ))
-        col.jenkins.http_client.open_and_read = self.mock_open_and_read
+        col = JenkinsCollector(mock_jenkins_client(self.url, self.mock_open_and_read),
+                               self.url,
+                               multibranch_pipeline_names=(multibranch_pipeline_name,))
         return col.collect()
 
     def mock_open_and_read(self, request_path):
         match = re.match("/job/(.*)/api/json", request_path)
         return read("multibranch/" + to_filename(match.group(1).strip("/")))
+
+
+def mock_jenkins_client(base_url, mock_open_and_read):
+    jenkins = JenkinsClient(http_client=HttpClient(base_url=base_url))
+    jenkins.http_client.open_and_read = mock_open_and_read
+    return jenkins
+
 
 if __name__ == '__main__':
     main()
